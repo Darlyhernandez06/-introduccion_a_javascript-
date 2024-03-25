868bcd25a3e2f7425a1fa303cb2eed8934b0425c
Darly Hernadez06 <darlyhernadez0624@gmail.com>
2024-03-25 16:21:56 -0500

    1. primer ejercicio

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,3 +1,7 @@
+// 1. Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor o
+// menor de edad, el programa debe validar que solo se puedan ingresar números positivos. 
+
+
 function validarEdad() {
     let edadInput = document.getElementById("edadInput").value;
     let resultado = document.getElementById("resultado");
